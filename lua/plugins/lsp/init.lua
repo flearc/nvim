285f1684a887b78e0ef743dc00ddef293ce685a5
flea <fleashiro@gmail.com>
2024-08-23 13:56:40 +0800

    feat: remove go.nvim
    lspconfig is good enough

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -8,21 +8,6 @@ return {
     { 'j-hui/fidget.nvim', opts = {} },
     -- setup rust
     { 'mrcjkb/rustaceanvim', version = '^4', lazy = false },
-    -- setup go
-    {
-      'ray-x/go.nvim',
-      dependencies = { -- optional packages
-        'ray-x/guihua.lua',
-        'neovim/nvim-lspconfig',
-        'nvim-treesitter/nvim-treesitter',
-      },
-      config = function()
-        require('go').setup()
-      end,
-      event = { 'CmdlineEnter' },
-      ft = { 'go', 'gomod' },
-      build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-    },
   },
   opts = {
     servers = {
