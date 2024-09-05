ea4a1863eb4666776278edcd2b055eaf9c8b8dc9
flea <fleashiro@gmail.com>
2024-09-05 18:39:07 +0800

    feat: change colorscheme to tokyonight

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,40 +1,11 @@
 return {
-  -- {
-  --   'folke/tokyonight.nvim',
-  --   lazy = false,
-  --   priority = 1000,
-  --   init = function()
-  --     vim.cmd.colorscheme 'tokyonight-moon'
-  --     vim.cmd.hi 'Comment gui=none'
-  --   end,
-  -- },
   {
-    'rebelot/kanagawa.nvim',
+    'folke/tokyonight.nvim',
     lazy = false,
     priority = 1000,
     init = function()
-      -- Can be one of
-      --  kanagawa-wave
-      --  kanagawa-dragon
-      --  kanagawa-lotus
-      vim.cmd.colorscheme 'kanagawa-wave'
+      vim.cmd.colorscheme 'tokyonight-moon'
+      vim.cmd.hi 'Comment gui=none'
     end,
   },
-  -- {
-  --   'ellisonleao/gruvbox.nvim',
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     vim.o.background = 'dark'
-  --     vim.cmd.colorscheme 'gruvbox'
-  --   end,
-  -- },
-  -- {
-  --   'catppuccin/nvim',
-  --   name = 'catppuccin',
-  --   priority = 1000,
-  --   config = function()
-  --     vim.cmd.colorscheme 'catppuccin-mocha'
-  --   end,
-  -- },
 }
