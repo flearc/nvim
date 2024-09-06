d19b68f600e45f8c2462f9ca2d677d3c348a91f1
flea <fleashiro@gmail.com>
2024-09-06 10:35:11 +0800

    track: kickstart.nvim #1064 #1068 #1108 #1113

--- lua/plugins/treebar.lua
+++ lua/plugins/treebar.lua
@@ -10,7 +10,7 @@ return {
     },
     cmd = 'Neotree',
     keys = {
-      { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal' },
+      { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
     },
     opts = {
       filesystem = {
--- plugin/autocmd.lua
+++ plugin/autocmd.lua
@@ -14,8 +14,9 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
   callback = function(event)
-    local map = function(keys, func, desc)
-      vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+    local map = function(keys, func, desc, mode)
+      mode = mode or 'n'
+      vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
     end
 
     -- Jump to the definition of the word under your cursor.
@@ -40,7 +41,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
 
     -- Execute a code action, usually your cursor needs to be on top of an error.
-    map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+    map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
 
     -- WARN: This is not Goto Definition, this is Goto Declaration.
     map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
