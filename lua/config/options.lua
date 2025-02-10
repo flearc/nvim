73c957b0ff442c08675daddca4ca1f630ee46290
flea <fleashiro@gmail.com>
2025-02-10 17:11:13 +0800

    new nvim config

---
+++ lua/config/options.lua
@@ -0,0 +1,12 @@
+local opt = vim.opt
+
+opt.inccommand = 'split'
+
+opt.smartcase = true
+opt.ignorecase = true
+
+opt.number = true
+opt.relativenumber = true
+
+opt.splitbelow = true
+opt.splitright = true
