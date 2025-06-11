4c1031a0183b4d1bf12399210c6fa1854b316236
fl <fleashiro@gmail.com>
2025-06-11 10:13:08 +0800

    add rust support & esc for terminal mode

--- lua/config/mapping.lua
+++ lua/config/mapping.lua
@@ -1,3 +1,4 @@
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 -- leetcode
 vim.keymap.set('n', '<leader>ld', '<cmd>Leet desc<cr>')
--- lua/plugins/lsp/conform.lua
+++ lua/plugins/lsp/conform.lua
@@ -32,6 +32,7 @@ return { -- Autoformat
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
+      rust = { 'rustfmt', lsp_format = 'fallback' },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
