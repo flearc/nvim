07ee0410dfb8c7e4e009c5e31319631406247e88
fl <fleashiro@gmail.com>
2025-06-19 14:21:25 +0800

    two keymap for open termianl at bottom and right

--- lua/config/mapping.lua
+++ lua/config/mapping.lua
@@ -1,4 +1,8 @@
+-- termianl
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set('n', '<leader>tv', '<cmd>vsplit | terminal<cr>', { desc = 'terminal on right' })
+vim.keymap.set('n', '<leader>ts', '<cmd>10split | terminal<cr>', { desc = 'terminal on bottom' })
+-- diagnostic
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 -- leetcode
 vim.keymap.set('n', '<leader>ld', '<cmd>Leet desc<cr>')
