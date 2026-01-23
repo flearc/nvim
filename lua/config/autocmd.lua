da1e80e484d5747e7613f0bd8cd8adeccfe81c45
fl <fleashiro@gmail.com>
2026-01-23 18:21:05 +0800

    add RestTemplate command to reset cf.cpp

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -6,3 +6,45 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.hl.on_yank()
   end,
 })
+
+-- reset cf.cpp
+local templates = {
+  ['cf.cpp'] = [[
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // WRITE_CODE_HERE
+
+    return 0;
+}
+]],
+}
+
+vim.api.nvim_create_user_command('ResetTemplate', function(opts)
+  local current_file = vim.fn.expand '%:t'
+  local template = templates[current_file]
+
+  if template then
+    vim.cmd '%delete _'
+    vim.fn.append(0, vim.split(template, '\n'))
+
+    local lines = vim.fn.getline(1, '$')
+    if type(lines) == 'string' then
+      lines = { lines }
+    end
+    for i, line in ipairs(lines) do
+      if line:find '// WRITE_CODE_HERE' then
+        vim.api.nvim_win_set_cursor(0, { i + 1, 0 }) -- 下一行
+        break
+      end
+    end
+
+    vim.cmd 'write'
+  else
+    print('✗ No template for current file: ' .. current_file)
+  end
+end, { desc = 'reset current file to template' })
